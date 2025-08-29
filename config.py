9ae87b415d50aa4339373d12c14484686e0cc4fb
Yusuf Nazarov <yusufnazarow20090505@gmail.com>
2025-08-29 15:07:08 +0500

    fix bug

--- config.py
+++ config.py
@@ -5,8 +5,13 @@ Bot_Tokken=Bot('8123461004:AAECdzXRdauqXninjDFMOhQ1BxBeaCc3big')
 
 Group_id=-4985710716
 
-db=DatabaseConfig(
-    user = 'postgres',
-    password='@#20090505@#',
-    db_name='Queu'
+
+
+db = DatabaseConfig(
+    user="postgres_we9r_user",
+    password="4ETQ5MVyx5Sz1PHi2Tg0mM8bwH1kRBD5",
+    db_name="postgres_we9r",
+    host="dpg-d2ona40gjchc73eviscg-a.oregon-postgres.render.com",
+    port=5432
 )
+
